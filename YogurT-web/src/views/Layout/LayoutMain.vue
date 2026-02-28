f7c13e9e6d690f7277a7902395223f8ce6c08121
fogsbridge <fogsbridge@gmail.com>
2026-02-28 10:41:06 +0800

    feat(PageBanner): 添加PageBanner 组件

--- YogurT-web/src/views/Layout/LayoutMain.vue
+++ YogurT-web/src/views/Layout/LayoutMain.vue
@@ -1,8 +1,8 @@
 <template>
-  <main class="max-w-8xl mx-auto p-2 sm:p-4 lg:p-6 min-h-3/5 flex flex-row gap-4">
-    <RouterView class="w-full" />
+  <main class="max-w-8xl mx-auto p-2 sm:p-4 lg:p-6 min-h-3/5 flex flex-row gap-4 isolate">
+    <RouterView class="grow" />
 
-    <RouterView name="sidebar" class="hidden md:block w-95" />
+    <RouterView name="sidebar" class="hidden lg:block w-70" />
   </main>
 </template>
 
