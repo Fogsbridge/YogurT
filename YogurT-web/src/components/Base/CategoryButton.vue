232a6e1030e5c92afc043dca08ab7c1bd76d1243
fogsbridge <fogsbridge@gmail.com>
2026-03-05 11:53:23 +0800

    feat(CategoryButton): 添加分类按钮组件

---
+++ YogurT-web/src/components/Base/CategoryButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button
+    class="rounded-full   bg-slate-400/20 hover:bg-slate-400/70 text-base-content/60 hover:text-white duration-300 cursor-pointer"
+    @click.stop="router.push({ name: 'categories', params: { categoryName: categoryName } })"
+  >
+    {{ categoryName }}
+  </button>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+
+const props = defineProps({
+  categoryName: { type: String, required: true }
+})
+
+const router = useRouter()
+</script>
+
+<style scoped>
+
+</style>
