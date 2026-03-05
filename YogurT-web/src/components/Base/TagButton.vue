293820bc9a660e646f33d6ef7b169c2706061679
fogsbridge <fogsbridge@gmail.com>
2026-03-05 11:52:47 +0800

    feat(TagButton): 添加标签按钮组件

---
+++ YogurT-web/src/components/Base/TagButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <span
+    class="rounded-full text-base-content/60 duration-300 cursor-pointer tag-btn-color"
+    :style="{'--color-tag': tag.color}"
+    @click.stop="router.push({ name: 'tags', params: { tagName: tag.name } })"
+  >
+    #{{ tag.name }}
+  </span>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+
+const props = defineProps({
+  tag: { type: Object, required: true }
+})
+
+const router = useRouter()
+</script>
+
+<style scoped>
+:root {
+  --color-tag: initial;
+}
+
+.tag-btn-color:hover {
+  background-color: var(--color-tag);
+  color: white;
+}
+</style>
