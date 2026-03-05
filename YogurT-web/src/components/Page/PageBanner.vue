072c6814849c93968aa28ac94d2bf80a107e4203
fogsbridge <fogsbridge@gmail.com>
2026-03-05 11:59:59 +0800

    chore: 调整亮色模式颜色变量，修改 PageBanner 组件 prop 名

--- YogurT-web/src/components/Page/PageBanner.vue
+++ YogurT-web/src/components/Page/PageBanner.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="-mb-20">
-    <div class="relative size-full bg-cover bg-center" :style="{ 'background-image': `url(${imgSrc})` }">
+    <div class="relative size-full bg-cover bg-center" :style="{ 'background-image': `url(${imgUrl})` }">
       <div class="absolute inset-0 bg-black/15 dark:bg-black/30 transition duration-500"></div>
       <div class="absolute inset-0 bg-linear-to-b from-transparent from-90% to-base-200 transition duration-500"></div>
       <div class="relative pt-15">
@@ -12,7 +12,7 @@
 
 <script setup>
 const props = defineProps({
-  imgSrc: { type: String, default: '/images/banner/banner-5.jpg' }
+  imgUrl: { type: String, default: '/images/banner/banner-5.jpg' }
 })
 </script>
 
