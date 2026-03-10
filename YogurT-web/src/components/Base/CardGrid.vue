d1f11f2b65ac4a2765a8950845e4265bdf256a4c
fogsbridge <fogsbridge@gmail.com>
2026-03-11 04:01:41 +0800

    feat(CardGrid): 添加卡片网格组件，支持传入动态的响应式的列数

---
+++ YogurT-web/src/components/Base/CardGrid.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul class="grid grid-cols-(--cols) md:grid-cols-(--md-cols) lg:grid-cols-(--lg-cols) xl:grid-cols-(--xl-cols) gap-(--gap)"
+      v-tw:--cols.grid-cols="cols"
+      v-tw:--md-cols.grid-cols="mdCols"
+      v-tw:--lg-cols.grid-cols="lgCols"
+      v-tw:--xl-cols.grid-cols="xlCols"
+      v-tw:--gap.spacing="gap"
+  >
+    <li v-for="(item, index) in items" :key="index">
+      <slot v-bind="{item, index}" />
+    </li>
+  </ul>
+</template>
+
+<script setup>
+const props = defineProps({
+  items: { type: Array },
+  gap: { type: Number, default: 4 },
+  cols: { type: Number, default: 1 },
+  mdCols: { type: Number, default: 1 },
+  lgCols: { type: Number, default: 2 },
+  xlCols: { type: Number, default: 2 }
+})
+</script>
+
+<style scoped>
+
+</style>
