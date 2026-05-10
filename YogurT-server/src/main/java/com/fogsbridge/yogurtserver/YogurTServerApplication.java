26dcffd764a23eb96fcfadc3bbffd0345e84d048
fogsbridge <fogsbridge@gmail.com>
2026-05-10 09:00:05 +0800

    feat: 引入 Flyway 数据库迁移工具，添加开发与生产环境配置，集成 MySQL 和 Flyway

--- YogurT-server/src/main/java/com/fogsbridge/yogurtserver/YogurTServerApplication.java
+++ YogurT-server/src/main/java/com/fogsbridge/yogurtserver/YogurTServerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class YogurTServerApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(YogurTServerApplication.class, args);
     }
 
